263a3be3a17ca871d65d2dd0332c053583e498ea
Antoine Villeret <antoine.villeret+dev@gmail.com>
2015-01-15 13:32:00 +0100

    update script to display 3 streams

--- RPi/local_display.sh
+++ RPi/local_display.sh
@@ -1,1 +1,4 @@
-gst-launch -v udpsrc port=5001 ! application/x-rtp, payload=96 ! rtph264depay ! ffdec_h264 ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 -v udpsrc port=5001 ! application/x-rtp, payload=96 ! rtph264depay ! ffdec_h264 ! ffmpegcolorspace ! autovideosink &
+gst-launch-0.10 -v udpsrc port=5002 ! application/x-rtp, payload=96 ! rtph264depay ! ffdec_h264 ! ffmpegcolorspace ! autovideosink &
+gst-launch-0.10 -v udpsrc port=5003 ! application/x-rtp, payload=96 ! rtph264depay ! ffdec_h264 ! ffmpegcolorspace ! autovideosink &
+
