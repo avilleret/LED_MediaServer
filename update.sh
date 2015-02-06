db8cbb283de5cfdfaf084f873a3339562d2e81f3
Antoine Villeret <antoine.villeret@gmail.com>
2015-02-06 21:10:16 +0100

    script pour les mises à jour du dépot git

---
+++ update.sh
@@ -0,0 +1,11 @@
+_DATE=$(date +%Y.%d.%m-%H.%M.%S)
+_DIR=$(dirname $(readlink -f $0))
+
+cd $_DIR
+echo "sauvegarde des modifications depuis la dernière fois"
+git checkout -b stage-$_DATE
+git commit -am "sauvegarde du $_DATE"
+git checkout master
+
+echo "récupère les mises à jour depuis l'internet"
+git pull
