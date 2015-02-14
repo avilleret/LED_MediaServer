393c1936952bc916794579ec270afa8176498423
Cyrille Henry <ch@chnry.net>
2015-02-14 10:50:31 +0100

    correct path for init script

--- RPi/init_RP2.sh
+++ RPi/init_RP2.sh
@@ -3,10 +3,10 @@
 sudo mount -o remount,rw /
 
 sudo rm /etc/init.d/codi
-sudo ln -s init.d/codi_2 /etc/init.d/codi
+sudo ln -s /home/pi/Corps_Diplomatique/RPi/init.d/codi_2 /etc/init.d/codi
 
 sudo rm read_button.py
 sudo ln -s read_button_2.py read_button.py
 
 sudo rm /etc/network/interfaces
-sudo ln -s network/interface_2 /etc/network/interfaces
+sudo ln -s /home/pi/Corps_Diplomatique/RPi/network/interfaces_2 /etc/network/interfaces
--- RPi/init_RP3.sh
+++ RPi/init_RP3.sh
@@ -3,10 +3,10 @@
 sudo mount -o remount,rw /
 
 sudo rm /etc/init.d/codi
-sudo ln -s init.d/codi_3 /etc/init.d/codi
+sudo ln -s /home/pi/Corps_Diplomatique/RPi/init.d/codi_3 /etc/init.d/codi
 
 sudo rm read_button.py
 sudo ln -s read_button_3.py read_button.py
 
 sudo rm /etc/network/interfaces
-sudo ln -s network/interface_3 /etc/network/interfaces
+sudo ln -s /home/pi/Corps_Diplomatique/RPi/network/interfaces_3 /etc/network/interfaces
