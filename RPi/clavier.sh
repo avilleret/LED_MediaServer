7d7a13e73e4f91fd86e9417235fdb1ae3f31c905
Cyrille Henry <ch@chnry.net>
2015-02-18 16:24:53 +0100

    deplace le patch pd ds un endrois plus logique

--- RPi/clavier.sh
+++ RPi/clavier.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 10
-sudo /home/pi/Corps_Diplomatique/RPi/bin/pd-0.46-4/bin/pd -noaudio -nomidi -nogui -open /home/pi/Corps_Diplomatique/RPi/bin/hid/clavier.pd
+sudo /home/pi/Corps_Diplomatique/RPi/bin/pd-0.46-4/bin/pd -noaudio -nomidi -nogui -open /home/pi/Corps_Diplomatique/RPi/clavier.pd
