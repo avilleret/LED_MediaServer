f70376c49215b02bb29e80fc1d056ae1a84e9ad4
Antoine Villeret <antoine.villeret+dev@gmail.com>
2015-02-25 11:42:26 +0100

    script executable + supression des commandes qui m'on fait perdre ce que j'ai fait ce matin

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@ _DATE=$(date +%Y.%d.%m-%H.%M.%S)
 _DIR=$(dirname $(readlink -f $0))
 
 cd $_DIR
-echo "sauvegarde des modifications depuis la dernière fois"
-git checkout -b stage-$_DATE
-git commit -am "sauvegarde du $_DATE"
-git checkout master
+# echo "sauvegarde des modifications depuis la dernière fois"
+# git checkout -b stage-$_DATE
+# git commit -am "sauvegarde du $_DATE"
+# git checkout master
 
 echo "récupère les mises à jour depuis l'internet"
 git pull
