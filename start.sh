fa1c6de85391ec51f90a130728f5b2eb0d13ed2d
Antoine Villeret <antoine.villeret+dev@gmail.com>
2016-06-11 13:05:01 +0200

    autostart script now calls manual start

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 _DIR=$(dirname $(readlink -f $0))
 sleep 30
-xterm -e "java -jar $_DIR/pixelpusher/PixelPusher_server.jar" &
-sleep 2
-$_DIR/RPi/raspicam2v4l2.sh
-cd $_DIR/pd
-pd LEDMediaServer_01.pd
+$_DIR/start_now.sh
